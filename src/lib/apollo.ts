793106b6ad914dba07d1f8fe9d3e07b1cb3e5e12
Matheus Guissi <guissimatheus@gmail.com>
2022-06-23 00:46:39 -0300

    Aula 3
    Adicionando Rotas
    Implementação de Video e conteúdo da Lesson

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,21 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+/**
+ * Remoção de Cache para solucionar o loading infinito entre os vídeos
+ */
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "ignore",
+    },
+    query: {
+        fetchPolicy: "no-cache",
+        errorPolicy: "all",
+    },
+};
 
 export const client = new ApolloClient({
     uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o9a17019fh01z7banbb1w9/master',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions
 })
