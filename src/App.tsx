db0e47ae83e6139ea55322a7522f908869520b78
Matheus Guissi <guissimatheus@gmail.com>
2022-06-27 02:16:54 -0300

    Correção urls para Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Router } from "./Router"
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter basename="/event-platform-react/">
+      <BrowserRouter>
         <Router />
       </BrowserRouter>
     </ApolloProvider>
